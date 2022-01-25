d5ce5399d07d9a5093a46f39e728da25c3d6e98f
xuefen376 <xuefen20100721@163.com>
2022-01-25 19:04:20 +0800

    项目第一阶段

---
+++ src/views/category/Category.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <ul class="content">
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+      <li>列表better-scroll</li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Category',
+  data() {
+    return {
+      scroll: null
+    }
+  },
+  mounted: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,119 @@
+<template>
+  <div id="home">
+      <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners" class="home-swiper"/>
+    <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"
+    @tabClick="tabClick"/>
+    <good-list :goods="showGoods" />
+   
+
+  </div>
+</template>
+
+<script>
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
+import GoodList from 'components/content/goods/GoodList.vue'
+
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+
+
+import {getHomeMultidata, getHomeGoods} from 'network/home'
+
+
+
+
+export default {
+  name: 'Home',
+  components: {  HomeSwiper, RecommendView,FeatureView,NavBar,TabControl,GoodList},
+  data(){
+    return {
+        banners: [],
+        recommends: [],
+        goods:{
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []},
+        },
+        currentIndex:'pop'
+          
+        
+    }
+  },
+  created() {
+    // 1.请求多个数据
+    this.getHomeMultidata()
+   // 2.请求商品数据
+   this.getHomeGoods('pop')
+   this.getHomeGoods('new')
+   this.getHomeGoods('sell')
+},
+computed: {
+  showGoods() {
+    return this.goods[this.currentIndex].list
+  }
+},
+methods: {
+  /**
+   * 事件监听相关的方法
+   */
+
+  tabClick(index) {
+    switch(index) {
+      case 0:  this.currentIndex='pop'
+      break
+      case 1: this.currentIndex='new'
+      break
+      case 2: this.currentIndex='sell'
+      break
+
+    }
+  },
+
+  /**
+   * 网络请求相关的方法
+   */
+  getHomeMultidata() {
+    getHomeMultidata().then(res =>{
+        console.log(res.data.data.banner.list);
+        this.banners = res.data.data.banner.list;
+        this.recommends = res.data.data.recommend.list;
+    })
+  },
+  getHomeGoods(type) {
+    const page = this.goods[type].page +1;
+    getHomeGoods(type, page).then(res =>{
+      console.log(res)
+        this.goods[type].list.push(...res.data.data.list)
+        this.goods[type].page +=1
+    })
+  }
+  }
+}
+
+</script>
+
+<style scoped>
+    .home-nav {
+      background-color: var(--color-tint);
+      color:#fff;
+
+      position: fixed;
+      left: 0;
+      right: 0;
+      top: 0;
+      z-index: 9;
+    }
+    .home-swiper {
+      padding-top: 44px;
+    }
+    .tab-control {
+      position:sticky;
+      top:44px;
+      z-index: 9;
+    }
+</style>
